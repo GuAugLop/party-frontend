eb84a6124ad46228ea061884722888241b246c5c
Gustavo Lopes <gulopes.augusto@gmail.com>
2021-05-22 17:44:34 -0300

    Update readme

--- src/components/PostModal/Comment.js
+++ src/components/PostModal/Comment.js
@@ -1,10 +1,15 @@
 import React from "react";
 import styles from "./Comment.module.css";
+import defaultAvatar from "../../static/img/defaultAvatar.png";
 
 const Comment = ({ comment }) => {
   return (
     <li className={styles.container}>
-      <img src={comment.user.thumb} alt="avatar" className={styles.avatar} />
+      <img
+        src={comment.user.thumb || defaultAvatar}
+        alt="avatar"
+        className={styles.avatar}
+      />
       <div className={styles.comment}>
         <p>{comment.user.username}:</p>
         <p>{comment.body}</p>
